95af64f7bbe866e952ac92da28a040adcb957698
Jeff Reiner <theundeadghost@gmail.com>
2017-04-03 16:14:33 -0400

    xo-ify
    Happiness linting!

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 *  gathered from the webhook payload
 *
 *  TO GET STARTED
-*  * Add your HubSpot API & Wistia API keys and uuid to .env file 
+*  * Add your HubSpot API & Wistia API keys and uuid to .env file
 *  * (Run a test webhook from Wistia to get the uuid of the payload)
 *  * Install dependencies via `npm install`
 *  * Run `node index.js` on a publicly visible IP
@@ -15,22 +15,30 @@
 */
 
 require('dotenv').config();
+const express = require('express');
+const rp = require('request-promise');
+const bodyParser = require('body-parser');
 
-var express     = require('express'),
-	rp          = require('request-promise'),
-	app         = express(),
-	bodyParser  = require('body-parser'),
-	port        = process.env.PORT || 3123,
-	wapiKey     = process.env.WAPI_KEY,
-	hapiKey 	= process.env.HAPI_KEY,
-	uuid        = process.env.UUID
+const app = express();
+const port = process.env.PORT || 3123;
+const wapiKey = process.env.WAPI_KEY;
+const hapiKey = process.env.HAPI_KEY;
+const uuid = process.env.UUID;
 
 // EASILY READ THE PAYLOAD FROM THE WEBHOOK
 app.use(bodyParser.json());
 
+// OBJECT TO THROW WHEN NOT AN ERROR
+function Thrower(message) {
+	this.name = 'Thrower';
+	this.message = message || 'Message';
+	console.log(message);
+}
+
 // VERIFY UUID FROM WISTIA PAYLOAD IS THE ONE WE WANT, IF NOT DON'T RUN API CALLS
+// NOTE: IN THE FUTURE MAKE THIS USE WISTIA'S SECRET KEY INSTEAD OF UUID FOR MATCHING. THAT WILL BE MORE FRIENDLY TO SET UP ANYWAYS
 function verify(req, res, next) {
-	if (req.body.hook.uuid == uuid) {
+	if (req.body.hook.uuid === uuid) {
 		next();
 	} else {
 		res.sendStatus(200);
@@ -42,63 +50,62 @@ function verify(req, res, next) {
 function hubstia(req, res, next) {
 	// VISITOR KEY COMES FROM WEBHOOK PAYLOAD FROM WISTIA
 	console.log('Wistia Visitor `id`: ' + req.body.events[0].payload.visitor.id);
-	
+
 	// VARS
-	var playCount = 0,
-		videosPlayed = 0,
-		contactEmail = 'bh@hubspot.com',
-		visitorKey = req.body.events[0].payload.visitor.id;
-	
+	let playCount = 0;
+	let	videosPlayed = 0;
+	let	contactEmail = 'bh@hubspot.com';
+	const visitorKey = req.body.events[0].payload.visitor.id;
+
 	// CALL TO WISTIA WITH KEY FROM WEBHOOK PAYLOAD TRIGGERED BY A VIDEO PLAY EVENT
-	var wistiaPath = 'https://api.wistia.com/v1/stats/visitors/' + visitorKey + '.json?api_password=' + wapiKey;
-	
+	const wistiaPath = 'https://api.wistia.com/v1/stats/visitors/' + visitorKey + '.json?api_password=' + wapiKey;
+
 	// CALL TO HUBSPOT WITH EMAIL FROM WISTIA STATS API RESPONSE DATA
-	var hubspotPath = 'https://api.hubapi.com/contacts/v1/contact/email/' + contactEmail + '/profile?hapikey=' + hapiKey;	
-		
-	/***
+	let hubspotPath = 'https://api.hubapi.com/contacts/v1/contact/email/' + contactEmail + '/profile?hapikey=' + hapiKey;
+
+	/*
 	 * NOTE: CALL TO WISTIA TO GET EMAIL FROM WEBHOOK
 	 */
-	 
+
 	rp({
 		method: 'GET',
 		uri: wistiaPath,
 		json: true,
 		resolveWithFullResponse: true
-	}).
-	then(function (response) {
-		/***
+	})
+	.then(response => {
+		/*
 		 * NOTE: GET VISITOR'S `play_count` AND `visitor_identity` EMAIL FROM WISTIA
 		 */
-		if (response.statusCode == 200) {
+		if (response.statusCode === 200) {
 			console.log('Wistia Visitor `play_count`: ' + response.body.play_count);
 			playCount = response.body.play_count;
-			
-			if (response.body.visitor_identity.email != null) {
-				/***
+
+			if (response.body.visitor_identity.email !== null) {
+				/*
 				 * NOTE: IF WISTIA VISITOR HAS AN EMAIL ATTACHED TO THEIR ID MAKE CALL TO HUBSPOT
 				 *       TO LOOKUP THEIR INFORMATION FROM THE WISTIA RESPONSE, THIS WON'T RUN IF
 				 *       THEY DON'T HAVE AN EMAIL, THAT WILL THEN END THE PROMISE CHAIN AND WE DONE
 				 */
 				contactEmail = response.body.visitor_identity.email;
 				console.log('Wistia Visitor `email`: ' + contactEmail);
-				hubspotPath = 'https://api.hubapi.com/contacts/v1/contact/email/' + contactEmail + '/profile?hapikey=' + hapiKey;	
-				
+				hubspotPath = 'https://api.hubapi.com/contacts/v1/contact/email/' + contactEmail + '/profile?hapikey=' + hapiKey;
+
 				return rp({
 					method: 'GET',
 					uri: hubspotPath,
 					json: true,
 					resolveWithFullResponse: true
 				});
-			} else {
-				throw 'No Email';
 			}
+			throw new Thrower('No Email');
 		} else {
 			throw new Error('Wistia GET Failed: ' + response.statusCode);
 		}
 	})
-	.then(function (response) {
-		if (response.statusCode == 200) {
-			/***
+	.then(response => {
+		if (response.statusCode === 200) {
+			/*
 			 * NOTE: PRINTS THE EMAIL IN THE HUBSPOT CONTACT RECORD. IF THE VALUE OF THE HUBSPOT CONTACT RECORD ISN'T SET
 			 *       THEN SET A DEFAULT VALUE OF 0. THIS VALUE (EITHER 0 OR X FROM WISTIA) IS THEN UPDATED INTO HUBSPOT IN THE FOLLOWING POST
 			 */
@@ -114,54 +121,53 @@ function hubstia(req, res, next) {
 			throw new Error('HubSpot GET Failed: ' + response.statusCode);
 		}
 	})
-	.then(function () {
+	.then(() => {
 		if (playCount > videosPlayed) {
-			/***
+			/*
 			 * NOTE: THIS WILL UPDATE THE HUBSPOT CONTACT RECORD WITH THE VALUE FROM WISTIA
 			 */
 			console.log('Setting HubSpot Record To Value From Wistia');
-			
+
 			videosPlayed = playCount;
-			
+
 			return rp({
 				method: 'POST',
 				uri: hubspotPath,
 				body: {
-					'properties': [
+					properties: [
 						{
-							'property': 'videos_played',
-							'value': videosPlayed
+							property: 'videos_played',
+							value: videosPlayed
 						}
 					]
 				},
 				json: true,
 				resolveWithFullResponse: true
 			});
-
-	 	} else if (playCount === videosPlayed) {
-			/***
+		} else if (playCount === videosPlayed) {
+			/*
 			 * NOTE: DON'T DO ANYTHING IF THE VALUES ARE THE SAME, THAT WOULD BE A WASTE OF A CALL
 			 */
 			console.log('Value\'s Are The Same');
-			
-			throw 'End Promise';
+
+			throw new Thrower('End Promise');
 		} else {
-			/***
+			/*
 			 * NOTE: THIS WILL INCREMENT THE HUBSPOT CONTACT RECORD IF IT IS HIGHER THAN THE VALUE FROM WISTIA.
 			 *       THIS IS MAINLY USED FOR WHEN WISTIA'S PLAY COUNT VALUE HASN'T BEEN UPDATED YET AS IT IS NOT REAL-TIME
 			 */
 			console.log('Manually Incrementing HubSpot Record');
-			
+
 			videosPlayed += 1;
-			
+
 			return rp({
 				method: 'POST',
 				uri: hubspotPath,
 				body: {
-					'properties': [
+					properties: [
 						{
-							'property': 'videos_played',
-							'value': videosPlayed
+							property: 'videos_played',
+							value: videosPlayed
 						}
 					]
 				},
@@ -170,21 +176,21 @@ function hubstia(req, res, next) {
 			});
 		}
 	})
-	.then(function (response) {
-		/***
+	.then(response => {
+		/*
 		 * NOTE: DISPLAYS THE FINAL HUBSPOT CONTACT RECORD OF `videos_played` AFTER IT HAS BEEN UPDATED EITHER MANUALLY OR BY WISTIA'S VALUE
-		 */ 
-		if(response.statusCode == 204) {
+		 */
+		if (response.statusCode === 204) {
 			console.log('Updated | HubSpot Contact `videos_played`: ' + videosPlayed);
 		} else {
 			throw new Error('HubSpot POST Failed: ' + response.statusCode);
 		}
 	})
-	.catch(function(err) {
-		/***
+	.catch(err => {
+		/*
 		 * NOTE: IF SOMETHING FAILS DURING THE PROMISE CHAIN OR ANYWHERE THIS BLOCK SHOULD FIRE WITH THE STACKTRACE OF THE ERROR
-		 */ 
-		if (err !== 'No Email' || err !== 'End Promise') {
+		 */
+		if (err.name !== 'Thrower') {
 			console.error(err);
 		}
 	});
@@ -194,18 +200,18 @@ function hubstia(req, res, next) {
 }
 
 // RUN BOTH FUNCTIONS UPON RECEIVING A POST TO THE SERVER
-app.post("/", verify, hubstia);
+app.post('/', verify, hubstia);
 
 // VISIBLE AT THE URL IN THE BROWSER DOESN'T DO ANYTHING THOUGH
-app.get('/', function(req, res) {
-	res.send('• Hubstia •')
+app.get('/', (req, res) => {
+	res.send('• Hubstia •');
 	console.log('GET Request Received');
-})
+});
 
 // STANDARD NODEJS LISTENER
-var server = app.listen(port, function () {
-	var host = server.address().address;
-	var port = server.address().port;
+const server = app.listen(port, () => {
+	const host = server.address().address;
+	const port = server.address().port;
 
 	console.log('Listening at https://%s:%s', host, port);
 });
